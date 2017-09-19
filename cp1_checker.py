5663265609e8fa11bbde673bfc52543583185b9a
nishkumar <nk595@cornell.com>
2017-09-19 02:34:47 -0400

    Fix server send bug

--- cp1_checker.py
+++ cp1_checker.py
@@ -19,6 +19,14 @@ numWritesReads = int(sys.argv[4])
 numBytes = int(sys.argv[5])
 numConnections = int(sys.argv[6])
 
+#Print params
+print "###############################"
+print "numTrials:",numTrials
+print "numWritesReads:",numWritesReads
+print "numBytes:",numBytes
+print "numConnections:",numConnections
+print "\n"
+
 if numConnections < numWritesReads:
     sys.stderr.write('<#connections> should be greater than or equal to <#writes and reads per trial>\n')
     sys.exit(1)
@@ -33,13 +41,16 @@ for i in xrange(numConnections):
     s = socket(AF_INET, SOCK_STREAM)
     s.connect((serverHost, serverPort))
     socketList.append(s)
-
+    
+print "Creating ", numConnections, " connections."
 
 for i in xrange(numTrials):
     socketSubset = []
     randomData = []
     randomLen = []
     socketSubset = random.sample(socketList, numConnections)
+    print "##### Trial",i ,"#####"
+    print "Sending"
     for j in xrange(numWritesReads):
         random_len = random.randrange(1, numBytes)
         random_string = os.urandom(random_len)
@@ -47,8 +58,10 @@ for i in xrange(numTrials):
         randomData.append(random_string)
         socketSubset[j].send(random_string)
 
+    print("Receiving")
     for j in xrange(numWritesReads):
         data = socketSubset[j].recv(randomLen[j])
+
         start_time = time.time()
         while True:
             if len(data) == randomLen[j]:
@@ -62,7 +75,10 @@ for i in xrange(numTrials):
             sys.stderr.write("Error: Data received is not the same as sent! \n")
             sys.exit(1)
 
+print "#################################### "
+print("Closing connections")
 for i in xrange(numConnections):
     socketList[i].close()
 
 print "Success!"
+print "#################################### \n"
